2df230e6d9d5b89bb7ee24a33e60e7c14f89bcfa
SilverSnake0 <105333801+SilverSnake0@users.noreply.github.com>
2022-12-14 01:26:40 -0500

    minor fix

--- main.py
+++ main.py
@@ -905,7 +905,7 @@ jgs .-=-.    ) -.
                     player_whackjack = True
                     current_bet *= 2
                     return player_whackjack, current_bet
-                elif player_total <= 11:
+                elif player_total == 11:
                     eleven_ans = input(f'Player hit a soft 11! Would you like to double down your bet and play one more hand?')
                 if eleven_ans.strip().lower() in (yes):
                     double_down = current_bet
@@ -2003,7 +2003,7 @@ jgs .-=-.    ) -.
                     if blackjack_ans.strip().lower() in yes:
                         input(
                             f'\n{bulletpoint2}(The goal is to get as close to 21 as possible. "Hit" will draw another card, while "Stand" will stop drawing)\nPress enter to continue...')
-                    while blackjack_ans.strip().lower() in yes or blackjack_ans.strip().lower() == '':
+                    while blackjack_ans.strip().lower() in yes:
                         blackjack()
                         blackjack_ans = input(
                             f'\n{bulletpoint2}(The goal is to get as close to 21 as possible. "Hit" will draw another card, while "Stand" will stop drawing)\n\n{bulletpoint2}(You can also press enter to play again)\n{bulletpoint}Play another round of Whackjack?\n')
